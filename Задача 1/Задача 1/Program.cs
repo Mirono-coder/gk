b1c4e9bb6cd8e04f32570437a20a8c3c894b0d73
urus <urus@URUS->
2021-09-10 08:17:02 +0300

    879o

---
+++ Задача 1/Задача 1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace Задача_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            oper oop = new oper();
+            Console.WriteLine("введите х");
+            string x1 = Console.ReadLine();
+            double x = Convert.ToDouble(x1);
+            Console.WriteLine("Введите у");
+            string x2 = Console.ReadLine();
+            double y = Convert.ToDouble(x2);
+            oop.sum(x, y);
+            oop.raz(x, y);
+            oop.pro(x, y);
+            oop.del(x, y);
+            Console.WriteLine();
+
+        }
+    }
+    class oper
+    {
+        double x;
+        double y;
+        public void sum(double x, double y)
+        {
+            x = x + y;
+            Console.WriteLine(x);
+
+        }
+        public void raz(double x, double y)
+        {
+            x = x - y;
+            Console.WriteLine(x);
+        }
+        public void pro(double x, double y)
+        {
+            x = x * y;
+            Console.WriteLine(x);
+        }
+        public void del(double x, double y)
+        {
+            x = x / y;
+            Console.WriteLine(x);
+
+        }
+    }
+}
